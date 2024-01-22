5c9adda9551355ff7799ac3db699866adc04d1e7
mijanurm21 <149139366+mijanurm21@users.noreply.github.com>
2024-01-22 19:47:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (5.08%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -23,20 +23,37 @@ public:
     // }
     // --------------------------------------------------
 
-    void solve(vector<int>nums, vector<int>&dp){
-        dp[0] = nums[0];
-        for(int i = 1; i<nums.size(); i++){
-            int pick = nums[i];
-            if(i>1) pick += dp[i-2];
-            int npick = 0 + dp[i-1];
+    // void solve(vector<int>nums, vector<int>&dp){
+    //     dp[0] = nums[0];
+    //     for(int i = 1; i<nums.size(); i++){
+    //         int pick = nums[i];
+    //         if(i>1) pick += dp[i-2];
+    //         int npick = 0 + dp[i-1];
 
-            dp[i] =  max(pick , npick);
-        }
-    }
+    //         dp[i] =  max(pick , npick);
+    //     }
+    // }
+    // -------------------------------------------------------
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1,-1);
-         solve( nums, dp);
-         return dp[n-1];
+        // vector<int> dp(n+1,-1);
+        //  solve( nums, dp);
+        //  return dp[n-1];
+
+
+
+        int prev2 = 0, prev = nums[0];
+
+        for(int i = 1; i<n; i++){
+            int pick = nums[i];
+            if(i>1) pick+=prev2;
+            int npick = 0 + prev;
+
+            int curi = max(pick , npick);
+            prev2 = prev;
+            prev = curi;
+
+        }
+        return prev;
     }
 };

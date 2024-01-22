06d15ab9809e9db66cfad46cd376bf0eb9e71da6
mijanurm21 <149139366+mijanurm21@users.noreply.github.com>
2024-01-22 19:15:24 +0530

    Time: 2 ms (40.63%) | Memory: 11.7 MB (5.08%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int nextnext = 0;
-        int next = 0;
-        int curr = 0;
-        for(int i = nums.size()-1; i>=0; i--){
-            int t = nums[i] + nextnext;
-            int n = 0+next;
+// ----------------------------------------------------------------------
+    // int solve(int n,vector<int>nums){
+    //     if(n == 0) return nums[n];
+    //     if(n<0) return 0;
+    //     int pick = nums[n] + solve(n-2, nums);   // RECURSION
+    //     int npick = 0 + solve(n-1, nums);
 
-            curr = max(t, n);
-            nextnext= next;
-            next =  curr;
-        }
-        return curr;
+    //     return max(pick ,npick);
+    // }
+    // ----------------------------------------------------------------------
+
+    int solve(int n, vector<int>nums, vector<int>&dp){
+        if(n == 0) return nums[n];
+        if(n<0) return 0;
+        if(dp[n] != -1)  return dp[n];
+
+        int pick = nums[n] + solve(n-2 ,nums, dp );
+        int npick = 0+ solve(n-1, nums, dp);
+        return dp[n] = max(pick , npick);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1,-1);
+        return solve(n-1, nums, dp);
     }
 };

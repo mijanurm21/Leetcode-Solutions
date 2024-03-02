d669572e0115559a61d0ba6187d2ce95461b6010
mijanurm21 <149139366+mijanurm21@users.noreply.github.com>
2024-03-02 12:05:43 +0530

    Time: 6 ms (79.61%) | Memory: 15.9 MB (39.74%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    int solve(int ind, int cap, vector<int>& prices, vector<vector<int>>&dp, int k){
+    // int solve(int ind, int cap, vector<int>& prices, vector<vector<int>>&dp, int k){
 
-        if(ind >= prices.size() || cap == 2*k) return 0;
-        if(dp[ind][cap] != -1) return dp[ind][cap];
-        int profit = 0;
-        if(cap % 2== 0) {
-            profit =  max(-prices[ind] + solve(ind+1, cap+1, prices, dp, k), solve(ind+1, cap, prices, dp,k) );
-        }
-        else{
-            profit = max(prices[ind] + solve(ind+1, cap+1, prices, dp, k), solve(ind+1, cap, prices,dp, k) );
-        }
-        return dp[ind][cap] = profit;
-    }
+    //     if(ind >= prices.size() || cap == 2*k) return 0;
+    //     if(dp[ind][cap] != -1) return dp[ind][cap];
+    //     int profit = 0;
+    //     if(cap % 2== 0) {
+    //         profit =  max(-prices[ind] + solve(ind+1, cap+1, prices, dp, k), solve(ind+1, cap, prices, dp,k) );
+    //     }
+    //     else{
+    //         profit = max(prices[ind] + solve(ind+1, cap+1, prices, dp, k), solve(ind+1, cap, prices,dp, k) );
+    //     }
+    //     return dp[ind][cap] = profit;
+    // }
 
 
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int> (2*k,-1));
-        return solve(0, 0, prices, dp, k);
+        vector<vector<int>> dp(n+1, vector<int> (2*k+1,0));
+        // return solve(0, 0, prices, dp, k);
+            // vector<int> after(2*cap+1,0);
+            // vector<int> curr(2*cap+1,0);
+
+        for(int ind = n-1; ind>=0; ind--){
+            for(int cap = (2*k)-1; cap>=0; cap--){
+                if(cap %2 ==  0) {
+                    dp[ind][cap] =  max(-prices[ind] + dp[ind+1][cap+1], dp[ind+1][cap]);
+                }
+                else {
+                    dp[ind][cap] = max(prices[ind] + dp[ind+1][cap+1], dp[ind+1][cap]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };

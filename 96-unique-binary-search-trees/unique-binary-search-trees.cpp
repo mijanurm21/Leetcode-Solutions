717d599fc9032a494b3d07e6d4db8f17e738840d
mijanurm21 <mijanurm119@gmail.com>
2024-03-11 18:04:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (13.99%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+int solve(int n, vector<int> &dp ){
+    if( n <= 0) return 1;
+    if(dp[n] != -1) return dp[n];
+    int ans = 0;
+
+    for(int i = 1; i<=n; i++){
+        ans+= solve(i-1, dp) * solve(n-i, dp);
+    }
+    return dp[n] = ans;
+}
+    int numTrees(int n) {
+        vector<int> dp(n+2, -1);
+        return solve(n, dp);
+    }
+};

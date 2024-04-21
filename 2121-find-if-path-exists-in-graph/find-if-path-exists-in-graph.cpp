48090dc287e8d3ad2949b504249a393bdad40b39
mijanurm21 <mijanurm119@gmail.com>
2024-04-21 15:14:47 +0530

    Time: 634 ms (36.27%) | Memory: 204.4 MB (35.18%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-
-        if(edges.size() == 0) return true;
+        
         unordered_map<int, vector<int>> m;
-        for(auto x: edges){
-            vector<int> temp = x;
 
-            int u = temp[0];
-            int v = temp[1];
-
-            m[u].push_back(v);
-            m[v].push_back(u);
-        }        
-
-        vector<bool>vis(n+1,false);
-
-        queue<int>q;
-        q.push(source);
-        vis[source] = true;
-
-        while(!q.empty()){
-            int temp = q.front();
-            q.pop();
-
-            vector<int> v = m[temp];
-            for(int i = 0; i<v.size(); i++){
-                int vertex = v[i];
-                if(vis[vertex] == false){
-                    q.push(vertex);
-                    vis[vertex] = true;
-                }
-            }
-            if(vis[destination])
-            return vis[destination];
+        for(auto it : edges){
+            m[it[0]].push_back(it[1]);
+            m[it[1]].push_back(it[0]);
+            
         }
 
-        return vis[destination];
+        vector<bool> visited(n+1, false);
+
+        queue<int> q;
+        q.push(source);
+        visited[source] = true;
+
+        while(!q.empty()){
+           int temp = q.front();
+           q.pop();
+
+           auto it =  m[temp];  // it m vector h
+           
+           for(int i = 0;i<it.size(); i++){
+                int node = it[i];
+                if(visited[node] == false){
+                    visited[node] = true;
+                    q.push(node);
+                }
+                if(visited[destination]) return visited[destination];
+           }
+
+
+        }
+        return visited[destination];
     }
 };
